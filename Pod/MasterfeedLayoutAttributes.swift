6f3eaec8d82f6b98ea7dd5b6f6fda730addb38e3
Felipe Ricieri <felipericieri@icloud.com>
2017-11-23 10:24:53 -0200

    Introducing layout resources

---
+++ Pod/MasterfeedLayoutAttributes.swift
@@ -0,0 +1,9 @@
+//
+//  MasterfeedLayoutAttributes.swift
+//  MasterfeedWaterfall
+//
+//  Created by Felipe Ricieri on 23/11/2017.
+//  Copyright © 2017 Ricieri ME. All rights reserved.
+//
+
+import Foundation
